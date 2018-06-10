c651c13e38a731cd2c3d2971e3aff12dc03d072d
PWWetterDotCom <spammarkus-social@yahoo.com>
2018-06-10 22:39:03 +0200

    Navbar begonnen

--- styles/navbar.php
+++ styles/navbar.php
@@ -2,26 +2,43 @@
 
     <section id="navbar">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Navbar w/ text</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-            </ul>
-            <span class="navbar-text">
-              Navbar text with an inline element
-            </span>
-          </div>
+            <div class="col-1 col-sm-1 col-md-1 col-lg-1">
+                <a class="navbar-brand" href="#">
+                    <img src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="Ghoster-Logo">
+                    Ghoster
+                </a>
+            </div>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Start</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gruppen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nachrichten</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Account (mach mich rechts...)
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Unterpunkt 1</a>
+                            <a class="dropdown-item" href="#">Unterpunkt 2</a>
+                            <a class="dropdown-item" href="#">Unterpunkt 3</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </section>
 
